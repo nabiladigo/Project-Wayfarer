fea6825869efadfb87eddecbbf415242c3970a70
j0shmartin <josh@joshmartin95.com>
2022-02-11 11:50:16 -0500

    user models and relevant urls and settings updates

--- wayfarer_django/wayfarer_project/settings.py
+++ wayfarer_django/wayfarer_project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'wayfarer_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'project-wayfarer',
     }
 }
