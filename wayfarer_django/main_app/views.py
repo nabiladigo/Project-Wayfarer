aaa8f5d9b51682430b2601fbd64501262959d864
Alan McGurney <mcgurneyalan@gmail.com>
2022-02-11 16:30:08 -0500

    more templates added about/base/home

--- wayfarer_django/main_app/views.py
+++ wayfarer_django/main_app/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
-class Home(View):
-     def get(self, request):
-         return HttpResponse("Wayfarer Home")
+class Home(TemplateView):
+     template_name = "home.html"
 
-class About(View):
-     def get(self, request):
-         return HttpResponse("Wayfarer bout")
+class About(TemplateView):
+     template_name = "about.html"
