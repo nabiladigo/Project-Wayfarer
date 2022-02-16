344e393d64be05f6812078a0f50160a7ac4e92fc
nabiladigo <nabiladigo91@gmail.com>
2022-02-16 17:01:07 -0500

    update views and urls

--- wayfarer_django/main_app/views.py
+++ wayfarer_django/main_app/views.py
@@ -1,7 +1,8 @@
+
 from django.shortcuts import redirect, render
 from django.views import View 
 from django.views.generic.base import TemplateView
-from .models import Country, City, Post, Comment, Profile
+from .models import Country, City, Post
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse
@@ -9,6 +10,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+
 
 
 # @method_decorator(login_required, name='dispatch')
@@ -38,6 +40,16 @@ class CityList(TemplateView):
         
         return context 
 
+class CityDetail(DetailView):
+    model = City
+    template_name = "city_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.filter(city_id= self.object.pk)
+        return context
+
+
 class Posts(TemplateView):
     template_name ="post.html" 
 
@@ -45,7 +57,7 @@ class Posts(TemplateView):
         context = super().get_context_data(**kwargs)
         name = self.request.GET.get("name")
         if name != None:
-            context["posts"] = Post.objects.ilter(name__icontains=name)
+            context["posts"] = Post.objects.filter(name__icontains=name)
             context["header"] = f"Searching for {name}"
         else:
             context["posts"] = Post.objects.all()
@@ -72,7 +84,7 @@ class PostDetail(DetailView):
 
 class PostUpdate(UpdateView):
     model = Post
-    fields = ['title','content','date', 'city']
+    fields = ['title','content', 'city']
     template_name = "post_update.html"
 
     def get_success_url(self):
@@ -84,6 +96,34 @@ class PostDelete(DeleteView):
     success_url = "/posts/"
 
 
+class CountryList(TemplateView):
+    template_name ="country.html" 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get("name")
+        if name != None:
+            context["countries"] = Country.objects.ilter(name__icontains=name)
+            context["header"] = f"Searching for {name}"
+        else:
+            context["countries"] = Country.objects.all()
+            context["header"] = "The Perfect place to visit."
+        
+        return context 
+
+class CountryDetail(DetailView):
+    model = Country
+    template_name ="country_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cities"] = City.objects.filter(country_id= self.object.pk)
+        print(self.object.pk)
+        return context
+
+class Profile(TemplateView):
+    template_name ="profile.html"
+    
 class Signup(View):
     def get(self, request):
         form = UserCreationForm()
@@ -99,27 +139,4 @@ class Signup(View):
         else:
             context = {"form": form}
             return render(request, "registration/signup.html", context)
-
-          
-class Profile(TemplateView):
-    template_name ="profile.html"
-
-class Caribbean(TemplateView):
-    template_name ="city.html"
-
-class Morocco(TemplateView):
-    template_name ="city.html"
-
-class France(TemplateView):
-    template_name = "city.html"
-
-class Ocean(TemplateView):
-    template_name  = "city.html"
-
-class Emirate(TemplateView):
-    template_name ="city.html"
-
-
-
-
 
